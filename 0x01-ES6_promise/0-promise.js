d0e7bdd2156b70e29a1997917bd8d528c5aaac24
chrisokwisa <okwisachristabel@gmail.com>
2022-12-21 04:32:31 -0800

    Added tasks one

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
-export default function getResponseFromAPI(){
-	const success = true;
-	return new Promise((resolve, reject) => {
-		if (success) {
-			resolve();
-		} else {
-			reject();
-		}
-	});
+export default function getResponseFromAPI() {
+  const success = true;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
