65454c14de378d65a9c8e56d562f9448b4b5f071
chrisokwisa <okwisachristabel@gmail.com>
2022-12-22 02:09:03 -0800

    Arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default functionn getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
