613e619eb6515ee156d58d4ecfc66ea3eebaf8b4
chrisokwisa <okwisachristabel@gmail.com>
2023-01-06 18:54:37 +0000

    Typed arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const arrayBuffer = new ArrayBuffer(length);
+  const typedArray = new Int8Array(arrayBuffer);
+  typedArray[position] = value;
+  return arrayBuffer;
+}
