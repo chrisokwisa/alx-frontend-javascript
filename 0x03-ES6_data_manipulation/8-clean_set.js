b5d0650cfa3c3d3924910a393b9188f316b29490
chrisokwisa <okwisachristabel@gmail.com>
2023-01-06 19:11:03 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,19 @@
 export default function cleanSet(set, startString) {
-  return [...set]
-    .filter(value => value.startsWith(startString))
-    .map(value => value.substring(startString.length))
-    .join('-');
+  const list = [];
+
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
+    }
+  }
+
+  return list.join('-');
 }
