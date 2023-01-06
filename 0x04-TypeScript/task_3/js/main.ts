7d465862e4f60dbaedde17255d5ec604a3f29518
chrisokwisa <okwisachristabel@gmail.com>
2023-01-06 20:50:31 +0300

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+const UpdatedRow: RowElement = { age: 23, ...row };
+
+CRUD.updateRow(newRowID, UpdatedRow);
+CRUD.deleteRow(newRowID);
